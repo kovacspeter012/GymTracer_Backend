9bc16deefc81a5f940afc8bfd72a6e3cd49f5ecf
Bende Huba <bende.huba@students.jedlik.eu>
2026-02-22 20:33:19 +0100

    Added migration to sync database structure to code

---
+++ Migrations/20260222192024_Token_Revoked_At_to_not_null.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymTracer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Token_Revoked_At_to_not_null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Tokens SET RevokedAt = '2025-01-01 00:00:00' WHERE RevokedAt IS NULL;");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RevokedAt",
+                table: "Tokens",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 9L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 10L,
+                column: "RevokedAt",
+                value: new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RevokedAt",
+                table: "Tokens",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 9L,
+                column: "RevokedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tokens",
+                keyColumn: "Id",
+                keyValue: 10L,
+                column: "RevokedAt",
+                value: null);
+        }
+    }
+}
