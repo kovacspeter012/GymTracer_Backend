86eefb8fda7d19e8bec058e9e9d5ecac553d85ce
Bende Huba <bende.huba@studenst.jedlik.eu>
2026-02-23 11:23:58 +0100

    Added BearerToken to authorization header, middleware now checks the BearerToken, added swagger support for BearerToken

---
+++ Auth/TokenHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace GymTracer.Auth
+{
+    public static class TokenHandler
+    {
+        public static string GenerateToken(int length)
+        {
+            return RandomNumberGenerator.GetHexString(128);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace GymTracer
 {
@@ -21,7 +23,7 @@ namespace GymTracer
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(AddSwaggerAuthButton);
             builder.Services.AddDbContext<GymTracerDbContext>(o =>
             {
                 o.UseMySQL(connString);
@@ -40,7 +42,7 @@ namespace GymTracer
                 {
                     options.ExpirationInMinutes = parsedMinutes;
                 }
-            });
+            }).AddBearerToken();
 
             builder.Services.AddAuthorization(options =>
             {
@@ -70,6 +72,39 @@ namespace GymTracer
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void AddSwaggerAuthButton(SwaggerGenOptions options)
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Unique Bearer Token API",
+                Version = "v1"
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "Unique",
+                In = ParameterLocation.Header,
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         }
     }
 }
