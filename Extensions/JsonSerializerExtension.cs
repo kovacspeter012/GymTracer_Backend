c61b58e247c9a3aac5c5f43cc161e42b905549b8
BendeHuba <bende.huba@students.jedlik.eu>
2026-02-25 13:40:31 +0100

    Created Dynamic (accessable from string) extension to Deserialize it (on null empty object is given) #30

---
+++ Extensions/JsonSerializerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GymTracer.Extensions
+{
+    public static class JsonSerializerExtension
+    {
+        public static readonly JsonSerializerOptions DefaultJsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        public static T Deserialize<T>(this string json) where T : new()
+        {
+            return JsonSerializer.Deserialize<T>(json, DefaultJsonSerializerOptions) ?? new T();
+        }
+
+    }
+}
