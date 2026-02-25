3d66606d6c06971f20028c30305ecfed1e38d65f
BendeHuba <bende.huba@students.jedlik.eu>
2026-02-25 13:58:35 +0100

    Reworked auth/registration auth/login and auth/logout routes to use new ControllerBase extension #1 #2 #3

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using GymTracer.Auth;
 using GymTracer.Context;
+using GymTracer.Extensions;
 using GymTracer.Models.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -29,26 +30,25 @@ namespace GymTracer.Controllers
         [HttpPost("registration")]
         public IActionResult Registration([FromBody] UserRegistrationDto? User)
         {
-
-            if(User is null || string.IsNullOrEmpty(User.Name) || string.IsNullOrEmpty(User.Email) || string.IsNullOrEmpty(User.Password))
-                return BadRequest(new { error = "Hibás adatok!" });
-
-            if(User.Password.Length < 8)
-                return BadRequest(new { error = "A jelszónak legalább 8 karakterből kell állnia" });
-
-            if(!EmailRegex().Match(User.Email).Success)
-                return BadRequest(new { error = "Az email címnek validnak kell lennie" });
-
-            if(User.Name.Trim() == "")
-                return BadRequest(new { error = "A névnek validnak kell lennie" });
-
-            var isUsedEmail = dbContext.Users.Any(u => u.Email == User.Email);
-            if(isUsedEmail)
-                return BadRequest(new { error = "Az email cím már használatban van" });
-
-            try
+            return this.Run(() =>
             {
-                var registeredUser = dbContext.Users.Add(new models.User()
+                if(User is null || string.IsNullOrEmpty(User.Name) || string.IsNullOrEmpty(User.Email) || string.IsNullOrEmpty(User.Password))
+                    return BadRequest(new { error = "Hibás adatok!" });
+
+                if(User.Password.Length < 8)
+                    return BadRequest(new { error = "A jelszónak legalább 8 karakterből kell állnia" });
+
+                if(!EmailRegex().Match(User.Email).Success)
+                    return BadRequest(new { error = "Az email címnek validnak kell lennie" });
+
+                if(User.Name.Trim() == "")
+                    return BadRequest(new { error = "A névnek validnak kell lennie" });
+
+                var isUsedEmail = dbContext.Users.Any(u => u.Email == User.Email);
+                if(isUsedEmail)
+                    return BadRequest(new { error = "Az email cím már használatban van" });
+
+                dbContext.Users.Add(new models.User()
                 {
                     Email = User.Email,
                     Name = User.Name,
@@ -57,79 +57,76 @@ namespace GymTracer.Controllers
                     Role = models.User_Role.customer,
                     Active = true,
                 });
-
                 dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-#if DEBUG
-                return BadRequest(new { ex.Message, ex.StackTrace });
-#else
-                return BadRequest(new { error = "Hibás adatok!" });
-#endif
 
-            }
-
-            return Ok(new { message = "Sikeres regisztráció!"} );
+                return Ok(new { message = "Sikeres regisztráció!"} );
+            });
         }
 
         [HttpPost("login")]
         public IActionResult Login([FromBody] UserLoginDto? User)
         {
-            if (User is null || string.IsNullOrEmpty(User.Email) || string.IsNullOrEmpty(User.Password))
-                return BadRequest(new { error = "Hibás adatok!" });
+            return this.Run(() =>
+            {
+                if (User is null || string.IsNullOrEmpty(User.Email) || string.IsNullOrEmpty(User.Password))
+                    return BadRequest(new { error = "Hibás adatok!" });
 
-            var dbUser = dbContext.Users.FirstOrDefault(u => u.Email == User.Email);
-            if (dbUser is null || !dbUser.Active || !passwordHandler.ComparePasswords(User.Password, dbUser.Password))
-                return BadRequest(new { error = "Hibás email cím vagy jelszó!" });
+                var dbUser = dbContext.Users.FirstOrDefault(u => u.Email == User.Email);
+                if (dbUser is null || !dbUser.Active || !passwordHandler.ComparePasswords(User.Password, dbUser.Password))
+                    return BadRequest(new { error = "Hibás email cím vagy jelszó!" });
 
-            tokenHandler.GenerateTokenData(out string tokenString, out DateTime createdAt, out DateTime revokedAt);
+                tokenHandler.GenerateTokenData(out string tokenString, out DateTime createdAt, out DateTime revokedAt);
 
-            dbContext.Tokens.Add(new models.Token {
-                UserId = dbUser.Id,
-                CreatedAt = createdAt,
-                RevokedAt = revokedAt,
-                TokenString = tokenString
-            });
-            dbContext.SaveChanges();
+                dbContext.Tokens.Add(new models.Token {
+                    UserId = dbUser.Id,
+                    CreatedAt = createdAt,
+                    RevokedAt = revokedAt,
+                    TokenString = tokenString
+                });
+                dbContext.SaveChanges();
 
-            return Ok(new {
-                message = "Sikeres bejelentkezés!",
-                user = new {
-                    id = dbUser.Id,
-                    name = dbUser.Name,
-                    email = dbUser.Email,
-                    role = dbUser.Role.ToString(),
-                },
-                token = tokenString,
-                validTo = revokedAt,
+                return Ok(new {
+                    message = "Sikeres bejelentkezés!",
+                    user = new {
+                        id = dbUser.Id,
+                        name = dbUser.Name,
+                        email = dbUser.Email,
+                        role = dbUser.Role.ToString(),
+                    },
+                    token = tokenString,
+                    validTo = revokedAt,
+                });
+
             });
         }
 
         [HttpPost("logout")]
         public IActionResult Logout()
         {
-            string? tokenString = User.Claims.FirstOrDefault(c => c.Type == "SessionToken")?.Value;
-
-            if(tokenString is null)
-                return BadRequest(new { error = "A token invalid!" });
-
-            models.Token? token = dbContext.Tokens.FirstOrDefault(t => t.TokenString == tokenString);
-
-            if (token is null)
-                return BadRequest(new { error = "A token invalid!" });
-
-            DateTime now = tokenHandler.Now();
-
-            // Ha még nem járt le a token
-            if (token.RevokedAt > now)
+            return this.Run(() =>
             {
-                token.RevokedAt = now;
-                dbContext.Update(token);
-                dbContext.SaveChanges();
-            }
+                string? tokenString = User.Claims.FirstOrDefault(c => c.Type == "SessionToken")?.Value;
 
-            return Ok(new { message = "Sikeres kijelentkezés!" });
+                if(tokenString is null)
+                    return BadRequest(new { error = "A token invalid!" });
+
+                models.Token? token = dbContext.Tokens.FirstOrDefault(t => t.TokenString == tokenString);
+
+                if (token is null)
+                    return BadRequest(new { error = "A token invalid!" });
+
+                DateTime now = tokenHandler.Now();
+
+                // Ha még nem járt le a token
+                if (token.RevokedAt > now)
+                {
+                    token.RevokedAt = now;
+                    dbContext.Update(token);
+                    dbContext.SaveChanges();
+                }
+
+                return Ok(new { message = "Sikeres kijelentkezés!" });
+            });
         }
     }
 }
