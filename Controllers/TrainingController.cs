24211a2c2675959e9d130f019079fce0a443a6e8
BendeHuba <bende.huba@students.jedlik.eu>
2026-02-26 13:06:14 +0100

    Updated post route training/user/{id} to further investigate the input data from user and to allow staff and admin to post trainings in others' names #18

--- Controllers/TrainingController.cs
+++ Controllers/TrainingController.cs
@@ -1,9 +1,11 @@
-﻿using GymTracer.Context;
+﻿using GymTracer.Auth;
+using GymTracer.Context;
 using GymTracer.Extensions;
 using GymTracer.models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace GymTracer.Controllers
@@ -14,9 +16,12 @@ namespace GymTracer.Controllers
     public class TrainingController : ControllerBase
     {
         private readonly GymTracerDbContext dbContext;
-        public TrainingController(GymTracerDbContext dbContext)
+        private readonly TokenHandler tokenHandler;
+
+        public TrainingController(GymTracerDbContext dbContext, TokenHandler tokenHandler)
         {
             this.dbContext = dbContext;
+            this.tokenHandler = tokenHandler;
         }
 
         [HttpPost("user/{id}")]
@@ -27,26 +32,46 @@ namespace GymTracer.Controllers
                 Training training = Training.Deserialize(body);
 
                 string? userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-                if(userId is null)
+                string? userRole = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+                if(userId is null || userRole is null)
                     return BadRequest("Hibás token!");
 
-                if(dbContext.Trainings.Any(t => t.StartTime == training.StartTime || t.EndTime == training.EndTime))
-                    return BadRequest("Ebben az időintervallumban már van regisztrált edzés!");
+                if(userId != id.ToString())
+                {
+                    if(userRole != nameof(User_Role.admin) && userRole != nameof(User_Role.staff))
+                        return BadRequest("Csak saját nevében hozhat létre edzést!");
 
-                if(training.Name.Trim() == "")
+                    User? selectedUser = dbContext.Users.FirstOrDefault(u => u.Id == id);
+                    if(selectedUser is null)
+                        return BadRequest("Nincs ilyen felhasználó!");
+                }
+
+                TimeSpan trainingTimeSpan = training.EndTime - training.StartTime;
+                if(training.StartTime < tokenHandler.Now() || training.EndTime < tokenHandler.Now())
+                    return BadRequest("Az edzés időpontja érvénytelen!");
+
+                if(trainingTimeSpan.TotalMinutes < 5)
+                    return BadRequest("Az edzés időtartama legalább 5 perc kell legyen!");
+                else if(trainingTimeSpan.TotalHours > 2)
+                    return BadRequest("Az edzés időtartama legfeljebb 2 óra lehet!");
+
+                if(string.IsNullOrEmpty(training.Name) || training.Name.Trim() == "")
                     return BadRequest("Az edzésnek valid névvel kell rendelkeznie!");
 
-                if(training.Description.Trim() == "")
+                if(string.IsNullOrEmpty(training.Description) || training.Description.Trim() == "")
                     return BadRequest("Az edzésnek valid leírással kell rendelkeznie!");
 
                 // TODO: Feltöltött kép validáció
-                if(training.Image.Trim() == "")
+                if(string.IsNullOrEmpty(training.Image) || training.Image.Trim() == "")
                     return BadRequest("Az edzésnek valid képpel kell rendelkeznie!");
 
                 if(training.MaxParticipant == 0)
                     return BadRequest("Az edzésnek legalább 1 résztvevővel kell rendelkeznie!");
 
-                Training dbTraining = new Training()
+                if(dbContext.Trainings.Any(t => t.StartTime < training.EndTime && training.StartTime < t.EndTime))
+                    return BadRequest("Ebben az időintervallumban már van regisztrált edzés!");
+
+                Training createdTraining = new Training()
                 {
                     Name = training.Name,
                     Description = training.Description,
@@ -54,11 +79,39 @@ namespace GymTracer.Controllers
                     StartTime = training.StartTime,
                     EndTime = training.EndTime,
                     MaxParticipant = training.MaxParticipant,
-                    TrainerId = long.Parse(userId), // TODO: Staff vagy admin létrehozhat más nevében
+                    TrainerId = id,
                     Active = true,
                 };
 
-                return Ok(training);
+                dbContext.Trainings.Add(createdTraining);
+                dbContext.SaveChanges();
+                Training dbTraining = dbContext.Trainings.Include(t=>t.Trainer).First(t => t.Id == createdTraining.Id);
+
+
+
+                return Ok(new
+                {
+                    message = "Az edzés sikeresen regisztrálva lett!",
+                    training = new
+                    {
+                        dbTraining.Id,
+
+                        dbTraining.Name,
+                        dbTraining.Description,
+                        dbTraining.Image,
+                        dbTraining.StartTime,
+                        dbTraining.EndTime,
+                        dbTraining.MaxParticipant,
+
+                        dbTraining.TrainerId,
+
+                        trainer = new
+                        {
+                            dbTraining.Trainer.Id,
+                            dbTraining.Trainer.Name
+                        }
+                    }
+                });
             });
         }
     }
