d0e4c9164536e69e7996432237b1d248107e5c2c
Kovács Péter <kovacs.peter@students.jedlik.eu>
2026-03-03 09:18:14 +0100

    route for getting all tickets #10

---
+++ Controllers/TicketController.cs
@@ -0,0 +1,36 @@
+﻿using GymTracer.Auth;
+using GymTracer.Context;
+using GymTracer.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymTracer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly GymTracerDbContext DbContext;
+
+        public TicketController(GymTracerDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTickets()
+        {
+            var tickets = DbContext.Set<Ticket>();
+
+            return StatusCode(200, tickets.Select(t => new
+            {
+                t.Id,
+                t.Type,
+                t.Description,
+                t.IsStudent,
+                t.Price,
+                t.MaxUsage
+            }));
+        }
+    }
+}
