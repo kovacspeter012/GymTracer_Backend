065e5652886956a34ddef66b7434d8203c9ec641
BendeHuba <bende.huba@students.jedlik.eu>
2026-03-13 08:35:02 +0100

    Created Validator that has its validation target as a field. On Validate method call, it returns a ValidatorChain with the input callback's property (and its name) #37

---
+++ DataValidator/Validator.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace GymTracer.DataValidator
+{
+    public class Validator<T>
+    {
+        private readonly T validationModel;
+        public Validator(T validatonTarget)
+        {
+            this.validationModel = validatonTarget;
+        }
+
+        public ValidatorChain<TProp> Validate<TProp>(Expression<Func<T, TProp>> expression)
+        {
+            var compiledFunction = expression.Compile();
+            var validationField = compiledFunction(validationModel);
+
+            var validationFieldName = ((MemberExpression)expression.Body).Member.Name;
+
+            return new ValidatorChain<TProp>(validationField, validationFieldName);
+        }
+    }
+}
