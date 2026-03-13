93c24ce25bb9a8a5e13c2d986a31f707dcd133ef
BendeHuba <bende.huba@students.jedlik.eu>
2026-03-13 12:13:00 +0100

    Changed all private fields to public properties in ValidatorChain to make ValidatorChain extendable #37

--- DataValidator/ValidatorChain.cs
+++ DataValidator/ValidatorChain.cs
@@ -2,29 +2,29 @@
 {
     public readonly struct ValidatorChain<TProp>
     {
-        private readonly TProp validationField;
-        private readonly string validationFieldName;
-        private readonly Dictionary<string, string> errors;
+        public TProp ValidationField { get; }
+        public string ValidationFieldName { get; }
+        public Dictionary<string, string> Errors { get; }
         public bool HasFailed { get; }
 
         public ValidatorChain(TProp validationField, string validationFieldName, Dictionary<string, string> errors, bool hasFailed = false)
         {
-            this.validationField = validationField;
-            this.validationFieldName = validationFieldName;
-            this.errors = errors;
+            this.ValidationField = validationField;
+            this.ValidationFieldName = validationFieldName;
+            this.Errors = errors;
             this.HasFailed = hasFailed;
         }
 
         public ValidatorChain<TProp> AddError(string message)
         {
-            errors.TryAdd(validationFieldName, message);
-            return new ValidatorChain<TProp>(validationField, validationFieldName, errors, true);
+            Errors.TryAdd(ValidationFieldName, message);
+            return new ValidatorChain<TProp>(ValidationField, ValidationFieldName, Errors, true);
         }
 
         public ValidatorChain<TProp> NotNull()
         {
-            if(!HasFailed && this.validationField is null)
-                return AddError($"A(z) {validationFieldName} mezőt meg kell adni");
+            if(!HasFailed && this.ValidationField is null)
+                return AddError($"A(z) {ValidationFieldName} mezőt meg kell adni");
 
             return this;
         }
