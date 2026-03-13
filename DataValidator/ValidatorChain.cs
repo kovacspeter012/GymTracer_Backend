1dd166ee51c7e03f0fc8db58c96ecd3c952aa407
BendeHuba <bende.huba@students.jedlik.eu>
2026-03-13 08:29:54 +0100

    Created ValidatorChain for Validation steps #37

---
+++ DataValidator/ValidatorChain.cs
@@ -0,0 +1,18 @@
+﻿namespace GymTracer.DataValidator
+{
+    public class ValidatorChain<TProp>
+    {
+        private readonly TProp validationField;
+        private readonly string validationFieldName;
+
+        public string Message { get; private set; } = string.Empty;
+        public bool IsValid { get; private set; } = true;
+
+        public ValidatorChain(TProp validationField, string validationFieldName)
+        {
+            this.validationField = validationField;
+            this.validationFieldName = validationFieldName;
+        }
+
+    }
+}
