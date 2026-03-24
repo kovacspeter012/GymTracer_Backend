f4fee9bfcfe7591f753cd49020bfc29d445a80ac
Kovács Péter <kovacs.peter@students.jedlik.eu>
2026-03-24 09:44:34 +0100

    controller init

---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,23 @@
+﻿using GymTracer.Auth;
+using GymTracer.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymTracer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+        private readonly GymTracerDbContext DbContext;
+        private readonly TokenHandler tokenHandler;
+
+        public StatisticController(GymTracerDbContext dbContext, TokenHandler tokenHandler)
+        {
+            this.DbContext = dbContext;
+            this.tokenHandler = tokenHandler;
+        }
+
+
+    }
+}
