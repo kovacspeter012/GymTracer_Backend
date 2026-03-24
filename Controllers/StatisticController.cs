85e77649063b5595d917e2b2d7f70f42315fddd4
Kovács Péter <kovacs.peter@students.jedlik.eu>
2026-03-24 10:56:05 +0100

    #23 gym statistic route finished

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -1,7 +1,11 @@
 ﻿using GymTracer.Auth;
 using GymTracer.Context;
+using GymTracer.Exceptions;
+using GymTracer.Extensions;
+using GymTracer.models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace GymTracer.Controllers
 {
@@ -18,6 +22,40 @@ namespace GymTracer.Controllers
             this.tokenHandler = tokenHandler;
         }
 
+        [HttpGet("gym")]
+        public IActionResult GetAttendanceStatistics([FromQuery] uint daysBack, [FromQuery] uint weeksBack)
+        {
+            return this.Run(() =>
+            {
+                if (daysBack > 7)
+                {
+                    throw new ApiException(400, "only 7 days back statistic allowed at max");
+                }
 
+                if (weeksBack > 4)
+                {
+                    throw new ApiException(400, "only 4 weeks back statistic allowed at max");
+                }
+
+                List<UsageLog> dayByStatistics = DbContext.Set<UsageLog>().Where(ul => ul.UseDate < tokenHandler.Now() && ul.UseDate > tokenHandler.Now().AddDays(-daysBack)).ToList();
+                var dayBackReturn = dayByStatistics.GroupBy(s => s.UseDate.Date).Select(g => new
+                {
+                    date = g.Key.ToString("yyyy.MM.dd"),
+                    guestNumber = g.Count()
+                });
+
+                List<UsageLog> weekByStatistics = DbContext.Set<UsageLog>().Where(ul => ul.UseDate < tokenHandler.Now() && ul.UseDate > tokenHandler.Now().AddDays(-weeksBack * 7)).ToList();
+                var weekBackReturn = weekByStatistics.GroupBy(s => new {Year = ISOWeek.GetYear(s.UseDate), Week = ISOWeek.GetWeekOfYear(s.UseDate)}).Select(g => new
+                {
+                    year = g.Key.Year,
+                    weekOfYear = g.Key.Week,
+                    guestNumber = g.Count()
+                });
+
+                return StatusCode(200, new { dayBackReturn, weekBackReturn });
+            });
+            
+
+        }
     }
 }
