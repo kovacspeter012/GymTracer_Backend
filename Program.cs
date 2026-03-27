6ee2edc4f5324072eb8dda0c0857a4f2bdf45150
Kovács Péter <kovacs.peter@students.jedlik.eu>
2026-03-27 10:16:12 +0100

    cors policy configured for angular

--- Program.cs
+++ Program.cs
@@ -16,9 +16,22 @@ namespace GymTracer
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
             var configuration = builder.Configuration;
             var connString = configuration.GetConnectionString("gymtracerDb");
             // Add services to the container.
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://localhost:4200")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                                  });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -66,6 +79,8 @@ namespace GymTracer
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
 
